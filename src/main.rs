9f23bd3d5a76e32b9d5b1414fdb9bee223e2be14
shuva <mahantashuva@gmail.com>
2026-01-15 03:09:12 +0530

    signup new subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use email_newsletter::configuration::get_configuration;
+use email_newsletter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
-
-use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listner = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
-    run(listner)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let listner = TcpListener::bind(address).expect("Failed to bind address");
+    run(listner, connection_pool)?.await
 }
